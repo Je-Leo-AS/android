02047484d93fb625251f1818fd153b293695ce44
Leo <leonardo_as20@hotmail.com>
2023-10-22 21:52:20 -0300

    termo

--- Termo/app/build.gradle.kts
+++ Termo/app/build.gradle.kts
@@ -3,12 +3,12 @@ plugins {
 }
 
 android {
-    namespace = "com.example.termo"
-    compileSdk = 33
+    namespace = "br.ufpr.delt.termo"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.termo"
-        minSdk = 21
+        applicationId = "br.ufpr.delt.termo"
+        minSdk = 24
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -19,10 +19,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
@@ -34,7 +31,7 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
