4083adc776c02dd9160c3997e9f088ab0cd5b941
Leo <leonardo_as20@hotmail.com>
2023-11-05 21:10:56 -0300

    texttospeech

--- TextToSpeech/app/src/main/java/com/example/texttospeech/MainActivity.java
+++ TextToSpeech/app/src/main/java/com/example/texttospeech/MainActivity.java
@@ -3,53 +3,99 @@ package com.example.texttospeech;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
 import android.speech.RecognizerIntent;
-import android.Manifest;
-
+import android.speech.tts.TextToSpeech;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnGenrateSpeech;
+
+    Button btnGenrateSpeech, btnGenerateText ,btnSave;
     protected static final int RESULT_SPEECH = 1;
-    Button btnGenerateText;
     EditText etText;
+    public List<String> items = new ArrayList<>();
+    public List<Tuple> Tuplas = new ArrayList<>();
+    ListView listView;
+    FirebaseFirestore firestore;
+    DocumentSnapshot documentSnapshot;
+    Tree tree = new Tree();
+    int contador = 0;
+
+    DocumentReference documentReference;
 
     TextToSpeech t1;
-    TextView tv;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //getSupportActionBar().hide();
         t1 = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @SuppressLint("SuspiciousIndentation")
             @Override
             public void onInit(int status) {
                 if (status != TextToSpeech.ERROR)
                     t1.setLanguage(Locale.ENGLISH);
-                //t1.setLanguage(Locale.getDefault());
-                // t1.setSpeechRate(1.0f);
             }
         });
-
+        firestore = FirebaseFirestore.getInstance();
+        listView = findViewById(R.id.listView);
         btnGenrateSpeech = findViewById(R.id.btn);
         btnGenerateText = findViewById(R.id.btnLis);
+        btnSave = findViewById(R.id.btnSave);
         etText = findViewById(R.id.et);
-        // tv = findViewById(R.id.textview);
+
+        CollectionReference collectionReference = firestore.collection("textos");
+        Task<QuerySnapshot> query = collectionReference.get();
+        query.addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                QuerySnapshot querySnapshot = task.getResult();
+
+                for (DocumentSnapshot document : querySnapshot)
+                    if (document.exists()) {
+                        String date = document.getString("date");
+                        String text = document.getString("text");
+
+                        // Combine os valores em uma única string com uma vírgula entre eles
+                        String combinedValue = text + ", " + date;
+                        items.add(combinedValue);
+                        contador++;
+
+                    }
+            } else {
+                Exception e = task.getException();
+                e.printStackTrace();
+            }
+        });
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
         btnGenrateSpeech.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,6 +119,54 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        ListView listView = findViewById(R.id.listView);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String stringSelecionada = (String) listView.getItemAtPosition(position);
+                String[] palavrasSplit = stringSelecionada.split(",");
+                String Frase = palavrasSplit[0].toString();
+                etText.setText(Frase);
+
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Date date = new Date();
+                String text = etText.getText().toString();
+                SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+                String dataFormatada = formatador.format(date);
+                Tuple item = new Tuple(text, dataFormatada);
+                Tuplas.add(item);
+                String sep = ",";
+                String item_new = text + sep + dataFormatada;
+                items.add(item_new);
+                final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, items);
+                listView.setAdapter(adapter);
+                Map<String, Object> map = item.toMap();
+                Item item1 = new Item("item" + contador, item);
+                tree.addItem(item1);
+                documentReference = firestore.collection("textos").document("textdata "+contador);
+
+                contador++;
+                documentReference.set(item).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(getApplicationContext(), "Mensagem enviada", Toast.LENGTH_SHORT);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Mensagem não enviada, error: " + e, Toast.LENGTH_SHORT);
+                    }
+                });
+
+            }
+        });
+
     }
 
     @Override
@@ -87,6 +181,69 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
+
 }
+class Tuple {
+    private String text;
+    private String date;
+
+    public Tuple(String text, String date) {
+        this.text = text;
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getDate() {
+        return date;
+    }
+    public Map<String, Object> toMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("text", this.text);
+        map.put("date", this.date);
+        return map;
+    }
+}
+
+class Item {
+    private String name;
+    private Tuple value;
+
+    public Item(String name, Tuple value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Tuple getValue() {
+        return value;
+    }
+}
+
+class Tree {
+    private List<Item> items;
+
+    public Tree() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+}
+void Start(){
+
+}
+
+
 
 
